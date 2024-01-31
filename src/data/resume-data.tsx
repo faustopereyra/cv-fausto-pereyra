2a11da81a567149e1a8e2ba6590938237f87323d
Fausto Pereyra <pfausto@live.com.ar>
2024-01-31 17:45:50 -0300

    Fix jib descriptions, added siliconmind

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -38,15 +38,28 @@ export const RESUME_DATA = {
   ],
   work: [
     {
+      company: "Silicon Mind",
+      link: "https://siliconmind.ai/",
+      badges: [],
+      title: "Founder & Lead AI Developer",
+      logo: "",
+      start: "2023",
+      end: "Present",
+      description: `I lead the creation of custom RAG AI applications, focusing on chatbots, appointment setters, and AI-driven content creation for businesses. I manage a team dedicated to leveraging the ChatGPT API and LangChain for developing creative solutions.`,
+    },
+    {
       company: "Faraday",
-      link: "", // Insert the URL of Faraday's website here
+      link: "https://faradaysec.com/",
       badges: [],
       title: "Sr. Frontend Developer (React)",
       logo: "", // Insert the variable name containing Faraday's company logo here
       start: "2021",
       end: "Present",
-      description:
-        "Key responsibilities and achievements in the role included leading the transition from Angular to React for the SaaS platform, enhancing user experience and frontend robustness. Worked on frontend development with a focus on cybersecurity, created user-friendly features aligned with UI/UX design goals, and transformed detailed wireframes into functional web interfaces. Mentored frontend developers on best practices and efficient coding. Technologies used: React.js, Cybersecurity, User Experience (UX), UI.",
+      description: `Led the switch from Angular to React for our SaaS platform at Faraday, focusing on a smoother user experience and more robust frontend.
+        Worked on front-end and kept an eye on cybersecurity aspects.
+        Focused on creating features that users love, keeping in line with our UI/UX design goals.
+        Turned detailed wireframes into functional and user-friendly web interfaces.
+        Enjoyed mentoring frontend developers, sharing knowledge on writing clean and efficient code, and exchanging ideas on best practices.`,
     },
     {
       company: "Acámica",
@@ -56,8 +69,8 @@ export const RESUME_DATA = {
       logo: "", // Insert the variable name containing Acámica's company logo here
       start: "2021",
       end: "2022",
-      description:
-        "Key responsibilities and achievements in the role included teaching advanced React concepts in a major Latin American bootcamp. Simplified complex topics such as Asynchronism, Promises/async await, and React Hooks. Employed a hands-on teaching approach with practical exercises and projects. Skills: React.js, Education, JavaScript, Communication.",
+      description: `Teach advanced React at a major bootcamp in Latin America, concepts like Asynchronism, Promises/async await, and React Hooks (useEffect, useContext).
+      Proud of seeing many of my students go on to do great things with the skills they’ve gained.`,
     },
     {
       company: "Aliwan",
@@ -67,8 +80,7 @@ export const RESUME_DATA = {
       logo: "", // Insert the variable name containing Aliwan's company logo here
       start: "2020",
       end: "2021",
-      description:
-        "Key responsibilities and achievements in the role included developing automation tools using Node.js and Python, enhancing team efficiency. Improved web performance by 50% using Node.JS caching and ReactJS. Designed and implemented a mail delivery system, increasing customer conversion. Managed the main server and coordinated with teams in Argentina and China. Skills: Automation, Web Performance, Email Marketing, Cross-Cultural Communication, E-Commerce.",
+      description: `Developed automation tools using Node.js and Python, boosting the communications team’s efficiency significantly.Enhanced web performance by 57% with Node.JS caching and ReactJS, focusing on efficient and responsive web solutions.Designed and implemented a mail delivery system, contributing to a 24% increase in customer conversion.Managed the main server and coordinated development efforts between the Argentina and China teams, ensuring smooth operations and effective communication.`,
     },
     {
       company: "Freelance",
@@ -78,8 +90,10 @@ export const RESUME_DATA = {
       logo: "", // Insert the variable name for your professional logo or personal branding here, if applicable
       start: "2018",
       end: "2021",
-      description:
-        "Key responsibilities and achievements in the role included developing full-stack web applications using the MERN stack, creating static websites with CSS/SASS and HTML, developing comprehensive and user-friendly Design Systems, and focusing on Shopify development for business performance optimization. Skills: Full-Stack Development, Front-end Development, Web Design, Shopify.",
+      description: `Experienced in developing full-stack web applications using the MERN stack. I enjoy turning complex problems into simple, beautiful, and intuitive designs.
+        Skilled in creating static websites with CSS/SASS and HTML, focusing on clean, efficient, and responsive designs.
+        Passionate about web design, I take ideas and turn them into comprehensive and user-friendly Design Systems.
+        In my Shopify development projects, I prioritize optimizing for higher conversions, always aiming to boost business performance.`,
     },
     {
       company: "Estudip",
@@ -89,8 +103,11 @@ export const RESUME_DATA = {
       logo: "", // Insert the variable name containing Estudip's company logo here
       start: "2016",
       end: "2020",
-      description:
-        "Key responsibilities and achievements in the role included founding and leading a web development and digital marketing agency. Transitioned from hands-on roles to managerial responsibilities. Implemented technical standards and ensured quality deliverables. Orchestrated marketing and management of a major event with 50,000 attendees. Managed website development, social media, and Facebook advertising campaigns. Skills: Entrepreneurship, Team Leadership, Web Development, Facebook Ads.",
+      description: `Founded a web development and digital marketing agency at 16, demonstrating exceptional entrepreneurial skills and a passion for technology and marketing.
+        Led and managed a small, dynamic team of developers and designers, transitioning from a hands-on role to a managerial position, effectively balancing leadership and technical responsibilities.
+        Spearheaded the creation and implementation of technical standards within the company, ensuring high-quality deliverables and efficient processes.
+        Successfully publicized a major event, attracting over 50,000 attendees, showcasing outstanding marketing and event management skills.
+        Directly involved in website development, social media setup, and Facebook advertising campaigns, significantly enhancing the digital presence of local real estate businesses.`,
     },
   ],
   skills: [
