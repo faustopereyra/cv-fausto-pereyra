9ee8e93768a663b5d4076f099255a976ceb1b6b3
Fausto Pereyra <pfausto@live.com.ar>
2024-01-31 04:40:24 -0300

    Added to setup

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,176 @@
-import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { GitHubIcon, LinkedInIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  name: "Fausto Pereyra",
+  initials: "FP",
+  location: "Buenos Aires, Argentina",
+  locationLink: "https://www.google.com/maps/place/Buenos+Aire",
+  about: "Sr Frontend Engineer & Professional Nerd",
+  summary: `Hello! I'm a tech enthusiast and seasoned Frontend Developer. My passion lies in solving complex challenges and refining processes to be effective and efficient.
+
+    Starting out at 16, I founded a media agency, diving into data-driven advertising and building comprehensive online business solutions. This early venture set the stage for my career in tech, where I now specialize in React development and streamlining web applications.
+    
+    My expertise spans React.js, Node.js, Python, and focusing on user-friendly UI/UX. I enjoy mentoring, sharing knowledge, and continuously learning.`,
+  avatarUrl: "https://avatars.githubusercontent.com/u/60635606?v=4",
+  personalWebsiteUrl: "https://www.faustopereyra.com/",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "fausto.pereyra@gmail.com",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/faustopereyra",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/fausto-pereyra/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Universidad Tecnológica Nacional",
+      degree: "Engineer's degree in Industrial Engineering",
+      start: "2018",
+      end: "2020",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
+      company: "Faraday",
+      link: "", // Insert the URL of Faraday's website here
+      badges: [],
+      title: "Sr. Frontend Developer (React)",
+      logo: "", // Insert the variable name containing Faraday's company logo here
       start: "2021",
-      end: "2024",
+      end: "Present",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Key responsibilities and achievements in the role included leading the transition from Angular to React for the SaaS platform, enhancing user experience and frontend robustness. Worked on frontend development with a focus on cybersecurity, created user-friendly features aligned with UI/UX design goals, and transformed detailed wireframes into functional web interfaces. Mentored frontend developers on best practices and efficient coding. Technologies used: React.js, Cybersecurity, User Experience (UX), UI.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
+      company: "Acámica",
+      link: "", // Insert the URL of Acámica's website here
+      badges: [],
+      title: "React Mentor",
+      logo: "", // Insert the variable name containing Acámica's company logo here
+      start: "2021",
+      end: "2022",
+      description:
+        "Key responsibilities and achievements in the role included teaching advanced React concepts in a major Latin American bootcamp. Simplified complex topics such as Asynchronism, Promises/async await, and React Hooks. Employed a hands-on teaching approach with practical exercises and projects. Skills: React.js, Education, JavaScript, Communication.",
+    },
+    {
+      company: "Aliwan",
+      link: "", // Insert the URL of Aliwan's website here
+      badges: [],
+      title: "Lead Software Engineer",
+      logo: "", // Insert the variable name containing Aliwan's company logo here
+      start: "2020",
       end: "2021",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Key responsibilities and achievements in the role included developing automation tools using Node.js and Python, enhancing team efficiency. Improved web performance by 50% using Node.JS caching and ReactJS. Designed and implemented a mail delivery system, increasing customer conversion. Managed the main server and coordinated with teams in Argentina and China. Skills: Automation, Web Performance, Email Marketing, Cross-Cultural Communication, E-Commerce.",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "Freelance",
+      link: "", // Insert your professional website or portfolio URL here if applicable
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      title: "Freelance Web Developer",
+      logo: "", // Insert the variable name for your professional logo or personal branding here, if applicable
+      start: "2018",
+      end: "2021",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        "Key responsibilities and achievements in the role included developing full-stack web applications using the MERN stack, creating static websites with CSS/SASS and HTML, developing comprehensive and user-friendly Design Systems, and focusing on Shopify development for business performance optimization. Skills: Full-Stack Development, Front-end Development, Web Design, Shopify.",
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
+      company: "Estudip",
+      link: "", // Insert the URL of Estudip's website here
       badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      title: "Software Engineer, Founder, Project Manager",
+      logo: "", // Insert the variable name containing Estudip's company logo here
+      start: "2016",
+      end: "2020",
+      description:
+        "Key responsibilities and achievements in the role included founding and leading a web development and digital marketing agency. Transitioned from hands-on roles to managerial responsibilities. Implemented technical standards and ensured quality deliverables. Orchestrated marketing and management of a major event with 50,000 attendees. Managed website development, social media, and Facebook advertising campaigns. Skills: Entrepreneurship, Team Leadership, Web Development, Facebook Ads.",
     },
   ],
   skills: [
+    "React.js",
+    "Cybersecurity",
+    "UI UX",
     "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Automation",
+    "Email Marketing",
+    "E-Commerce",
+    "Facebook Ads",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Siliconmind.ai",
+      techStack: ["Side Business", "Langchain", "Next.js"],
+      description: "An AI consultany agency",
+      logo: "",
+      link: {
+        label: "siliconmind.com",
+        href: "https://siliconmind.ai/",
+      },
+    },
+    {
+      title: "TutorIA",
       techStack: [
         "Side Project",
         "TypeScript",
         "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Langchain",
+        "Vector Database",
       ],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+        "Your personal AI Tutor, Upload your notes let the AI teach you",
+      logo: "",
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "tutoria.siliconmind.ai",
+        href: "https://tutoria.siliconmind.ai/",
       },
     },
     {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+      title: "Faustopereyra.com",
+      techStack: ["Side Project", "Next.js"],
+      description: "My personal website and newsletter. Built with Next.js",
+      logo: "",
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "faustopereyra.com",
+        href: "https://www.faustopereyra.com/",
       },
     },
     {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      title: "AI Youtube Podcast",
+      techStack: ["Side Project", "Podcast", "Youtube"],
       description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
+        "Youtube channel where I talk abot the latest AI tools and consepts",
+      logo: "",
       link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
+        label: "youtube.com",
+        href: "https://www.youtube.com/channel/UClOEKlL-SKvMWGLGbOAFsWQ",
       },
     },
     {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
+      title: "AI QR Generator",
+      techStack: ["Side Project", "Next.js", "AI", "Stable Diffusion"],
+      description: "Generates beautiful QR codes using AI",
+      logo: "",
       link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
+        label: "qr.siliconmind.ai",
+        href: "https://qr.siliconmind.ai/",
       },
     },
     {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
+      title: "Pipe Write",
+      techStack: ["WIP"],
+      description: "Create custom content creation pipelines with AI",
+      logo: "",
       link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
+        label: "",
+        href: "",
       },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
